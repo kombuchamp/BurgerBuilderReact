c5231b1405454b76533148e8ef78d5eb87e1c324
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-02 01:51:18 +0300

    Add dynamic burger building

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css';
 
 const burger = props => {
+    const ingredientsArray = Object.keys(props.ingredients).map(igKey => {
+        const igCount = props.ingredients[igKey];
+        return [...Array(igCount)].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />);
+    });
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredientsArray}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
