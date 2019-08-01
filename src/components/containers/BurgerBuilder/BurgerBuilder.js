5f3008319845fa845db965b94c26077514437392
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-02 02:41:48 +0300

    Add BuildControls

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Burger from '../../Burger/Burger';
+import BuildControls from '../../Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     constructor(props) {
@@ -7,10 +8,10 @@ class BurgerBuilder extends Component {
 
         this.state = {
             ingredients: {
-                salad: 1,
-                bacon: 1,
-                cheese: 2,
-                meat: 2,
+                salad: 0,
+                bacon: 0,
+                cheese: 0,
+                meat: 5,
             },
         };
     }
@@ -19,7 +20,7 @@ class BurgerBuilder extends Component {
         return (
             <>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
             </>
         );
     }
