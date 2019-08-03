f27bd73d190a81a302be72cb7a5343587f1f094e
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-04 00:18:33 +0300

    Turn modal into class component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-const Modal = props => (
-    <>
-        <Backdrop isOpen={props.isOpen} onClick={props.closeHandler} />
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.isOpen ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.isOpen ? '1' : '0',
-            }}
-        >
-            {props.children}
-        </div>
-    </>
-);
-
+class Modal extends Component {
+    render() {
+        return (
+            <>
+                <Backdrop isOpen={this.props.isOpen} onClick={this.props.closeHandler} />
+                <div
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.isOpen ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.isOpen ? '1' : '0',
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </>
+        );
+    }
+}
 export default Modal;
