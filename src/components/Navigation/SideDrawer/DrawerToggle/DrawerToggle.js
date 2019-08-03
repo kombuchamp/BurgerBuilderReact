196bb39e5c74b21e8ef655ab15585a6c963d486d
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-04 00:06:16 +0300

    Add DrawerToggle

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './DrawerToggle.module.css';
 
 const DrawerToggle = props => {
-    return <div onClick={props.onClick}>MENU</div>;
+    return (
+        <div className={styles.DrawerToggle} onClick={props.onClick}>
+            <div />
+            <div />
+            <div />
+        </div>
+    );
 };
 
 export default DrawerToggle;
