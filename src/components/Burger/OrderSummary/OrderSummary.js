aeccedfaaca663011c8995297b82916704aa02d2
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-04 00:15:09 +0300

    Turn OrderSummary into class component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 import styles from './OrderSummary.module.css';
 
-const OrderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => (
-        <li key={igKey + props.ingredients[igKey]}>
-            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-    ));
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => (
+            <li key={igKey + this.props.ingredients[igKey]}>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>
+        ));
 
-    return (
-        <>
-            <h3 className={styles.Header}>Your order</h3>
-            <p>Burger with:</p>
-            <ul>{ingredientSummary}</ul>
-            <p>
-                Total Price: <strong>{props.price.toFixed(2)}$</strong>
-            </p>
-            <p>Proceed to checkout?</p>
-            <Button type="danger" onClick={props.cancelButtonHandler}>
-                CANCEL
-            </Button>
-            <Button type="success" onClick={props.okButtonHandler}>
-                OK
-            </Button>
-        </>
-    );
-};
+        return (
+            <>
+                <h3 className={styles.Header}>Your order</h3>
+                <p>Burger with:</p>
+                <ul>{ingredientSummary}</ul>
+                <p>
+                    Total Price: <strong>{this.props.price.toFixed(2)}$</strong>
+                </p>
+                <p>Proceed to checkout?</p>
+                <Button type="danger" onClick={this.props.cancelButtonHandler}>
+                    CANCEL
+                </Button>
+                <Button type="success" onClick={this.props.okButtonHandler}>
+                    OK
+                </Button>
+            </>
+        );
+    }
+}
 
 export default OrderSummary;
