8043ccb18b3623ec29448f370d6f1ebf604c3114
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-20 00:05:14 +0300

    Remove commented out code

--- src/components/Order/Checkout/Checkout.js
+++ src/components/Order/Checkout/Checkout.js
@@ -5,31 +5,6 @@ import ContactData from '../ContactData/ContactData';
 import { connect } from 'react-redux';
 
 class Checkout extends Component {
-    // constructor(props) {
-    //     super(props);
-
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const params = query.entries();
-    //     const ingredients = {
-    //         meat: 0,
-    //         salad: 0,
-    //         bacon: 0,
-    //         cheese: 0,
-    //     };
-    //     for (let param of params) {
-    //         if (param[0] === 'price') continue; // workaround
-    //         ingredients[param[0]] = +param[1];
-    //     }
-    //     // Redirect to start page if no ingredients provided
-    //     if (Object.values(ingredients).reduce((sum, val) => (sum += val)) <= 0) {
-    //         this.props.history.replace('/');
-    //     }
-    //     this.state = {
-    //         ingredients,
-    //         totalPrice: query.get('price'),
-    //     };
-    // }
-
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     };
